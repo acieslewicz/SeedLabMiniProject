5d381da565bb6b00f089cd4c5cc1e4448b464a5d
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2019-09-28 19:52:25 -0600

    Adding LCD i2c communication to project

---
+++ MiniProjectCom.py
@@ -0,0 +1,34 @@
+import time
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+def configure_lcd():
+    lcd_columns = 16
+    lcd_rows = 2
+    
+    # Initialise I2C bus.
+    i2c = busio.I2C(board.SCL, board.SDA)
+    
+    # Initialise the LCD class
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+    lcd.clear()
+    lcd.color = [0, 120, 180]
+    time.sleep(1)
+    return lcd
+    
+def write_messages(lcd, message1="", message2=""):
+    if message1 is not None:
+        message1 = str(message1)
+    
+    if message2 is not None:
+        message2 = str(message2)
+    
+    message = message1 + "\n" + message2
+
+    lcd.message = message
+    
+def turn_lcd_off(lcd):
+    lcd.clear()
+    lcd.color = [0,0,0]
+    time.sleep(1)
