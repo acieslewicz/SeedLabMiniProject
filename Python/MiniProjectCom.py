0db54ed17ad6596c7f058c80e7d1ad342440c04c
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2019-10-04 12:43:56 -0600

    COmpleted remained of python code documentation

--- Python/MiniProjectCom.py
+++ Python/MiniProjectCom.py
@@ -1,10 +1,46 @@
+"""
+Mini Project OpenCV and communication integration
+========
+
+Handles configuring and controlling LCD in addition to configuring i2c bus 
+
+Look how easy it is to use:
+
+    Import as a module in order to access the functions inside
+	
+Credits
+--------
+Alexander Cieslewicz
+
+Features
+--------
+
+- Configures i2c bus and 2x16 LCD display
+- Writes 2 line messages to lcd screen
+- Turns off lcd
+
+Dependencies
+------------
+
+- smbus
+- busio
+- board
+- time
+- adafruit_character_lcd.character_lcd_rgb_i2c
+
+Support
+-------
+If there are any issues email us at: acieslewicz@mymail.mines.edu
+"""
+
 import smbus
 import time
 import board
 import busio
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
-#Function to configure the comunication between the arduino and the Raspberry Pi
+
 def configure_communication():
+    """Configures i2c devices and bus for communication with LCD and Arduino"""
     lcd_columns = 16
     lcd_rows = 2
     
@@ -20,19 +56,31 @@ def configure_communication():
     lcd.color = [0, 100, 0]
     time.sleep(1)
     return bus, i2c, lcd
-    #Function to configure the messages writted to the LCD
+
 def write_messages(lcd, message1="", message2=""):
+    """Prints 2 line messages on a 2x16 adafruit lcd
+    
+    Keyword arguments:
+    lcd -- lcd object to display on
+    message1 -- desired message for top line of lcd
+    message2 -- desired message for bottom line of lcd
+    """
+    #Format the two messages so that they are displayed as desired on the LCD
     if message1 is not None:
         message1 = str(message1)
     
     if message2 is not None:
         message2 = str(message2)
-    #Concatinqtes the message
     message = message1 + "\n" + message2
 
     lcd.message = message
-    #Function to turn the LCD off
+
 def turn_lcd_off(lcd):
+    """Clears and turns off the LCD
+    
+    Keyword arguments:
+    lcd -- lcd object to turn off
+    """
     lcd.clear()
     lcd.color = [0,0,0]
     time.sleep(1)
